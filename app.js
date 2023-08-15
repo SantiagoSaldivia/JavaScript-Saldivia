bdaf5126f1a7f8899656f58af04865d2dc25556a
SantiagoSaldivia <saldiviasantiago7@gmail.com>
2023-08-15 18:53:55 -0300

    Tercera Preentrega

--- app.js
+++ app.js
@@ -1,48 +1,11 @@
-alert("Bienvenidos a la calculadora de Promedios")
-const usuarioHabilitado = "Santiago"
-const contraseniaHabilitada = "Hola1234"
-
-
-
-if(usuario === usuarioHabilitado && contrasenia === contraseniaHabilitada){
-    alert ("Bienvenido " + usuario)
-
-    let cantidadAlumnos = parseInt(prompt("Ingrese la cantidad de estudiantes a los que quiera calcular el promedio"))
-
-    for(let i = 0 ; i < cantidadAlumnos ; i++){
-
-        let nombreEstudiante = prompt("Ingrese el nombre del alumno");
-        let nota1 = parseInt(prompt("Ingrese la calificación 1"));
-        let nota2 = parseInt(prompt("Ingrese la calificación 2"));
-        let nota3 = parseInt(prompt("Ingrese la calificación 3"));
-        let promedio = calcularPromedio(nota1, nota2, nota3);
-        alert("El promedio de " + nombreEstudiante + " es " + promedio);
-
-        let aprueba = apruebaAnio(promedio)
-
-        if(aprueba){
-            alert (nombreEstudiante + " aprueba de año");
-        } else{
-            alert (nombreEstudiante + " no aprueba el año")
-        }
+const header = document.querySelector("#header");
+const contenedor = document.querySelector("#contenedor");
+const body = document.querySelector("body");
+window.addEventListener("scroll", function(){
+    if(contenedor.getBoundingClientRect().top<10){
+        header.classList.add("scroll")
     }
-} else{
-    alert("Usuario o contraseña incorrecta. Intente de nuevo")
-}
-
-
-//Funciones 
-
-
-function calcularPromedio(nota1, nota2, nota3){
-    let promedio = (nota1 + nota2 + nota3)/ 3;
-    return promedio;
-}
-
-function apruebaAnio(promedio){
-    if(promedio >= 6){
-        return true;
-    } else {
-        return false;
+    else{
+        header.classList.remove("scroll")
     }
-}
+})
